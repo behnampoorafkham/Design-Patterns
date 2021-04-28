e9885e254871e8b6b13878ffa1b430b53ee208b4
behnam <behnampoorafkham@yahoo.com>
2021-04-28 11:25:58 +0430

    Abstract Factory Design pattern

---
+++ src/SE/Creational/AbstractFactory/Rectangle.java
@@ -0,0 +1,8 @@
+package SE.Creational.AbstractFactory;
+
+public class Rectangle implements Shape {
+   @Override
+   public void draw() {
+      System.out.println("Inside Rectangle::draw() method.");
+   }
+}
